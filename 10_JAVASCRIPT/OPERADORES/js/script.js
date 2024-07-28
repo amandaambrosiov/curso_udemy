4b8941e01cec4a6a2ac02304b284eaf43a3381f0
AmandaAmbrosioUchoa <amandaambrosio295@gmail.com>
2024-07-28 10:04:38 -0300

    subindo aula de js

---
+++ 10_JAVASCRIPT/OPERADORES/js/script.js
@@ -0,0 +1,106 @@
+//NUMBER -------------------------------------------------
+// console.log(2);
+// console.log(5.14);
+// console.log(-27);
+// console.log(typeof 2);
+// console.log(typeof 5.14);
+// console.log(typeof -27);
+
+//OPERAÇÕES ARITMETICAS -------------------------------------
+// console.log(2 + 4);
+// console.log(55 - 44 );
+// console.log(5 * 3);
+// console.log(10 / 2);
+
+// console.log(5 + (2 * 4))
+
+//ESPECIAL NUMBERS -------------------------------------
+// console.log(typeof Infinity);
+
+// console.log(typeof -Infinity);
+
+// console.log(12 * "asd");
+
+// console.log(typeof NaN)
+
+//STRING ----------------------------------------------
+// console.log("Um texto");
+// console.log('Mais um texto');
+// console.log(`13`);
+
+// SIMBOLOS ESPECIAIS ----------------------------------
+// console.log("testando a quebra de linhas \n duas linhas");
+
+// console.log("Espaçamento \t de tab");
+
+
+// CONCATENAÇÃO ---------------------------------------------
+// console.log("Oi," + " tudo" + " bem?");
+
+// INTERPOLAÇÃO (deve se usar crase) ------------------------ 
+// console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+// console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// BOOLEANOS (TRUE OR FALSE) ---------------------------------
+// console.log(true);
+
+// console.log(5 > 20);
+
+// console.log(30 > 20);
+
+// console.log(typeof false);
+
+// COMPARAÇÕES ---------------------------------------------
+
+// maior ou menor (> <) -------------
+// console.log( 5 <= 5);
+// console.log(5 < 5);
+
+// igual (==) ----------------------
+// console.log(10 == 10);
+// console.log(10 == 9);
+
+// diferente(!=) -----------------------
+// console.log(10 != 10);
+// console.log(10 != 9);
+
+// COMPARAÇÃO DE IDENTICO -----------------------------------
+// console.log(9 == "9");
+// console.log(9 + "9"); //erro 
+
+// console.log(9 === "9");
+
+// console.log(9 != "9");
+
+// console.log(9 !== "9");
+
+// OPERADORES LÓGICAS ---------------------------------------
+// && - and - true apenas d=se os dois lados forem vedadeiros
+// || - or - para ser true, um lado como true é suficiente
+// ! - not - este operador inverte a comparação
+
+// console.log(true && true);
+
+// console.log(true && false);
+
+// console.log(5 > 2 && 2 < 10);
+
+// console.log(5 > 2&& "Matheus" === 1);
+
+// console.log(5 > 2 || "Matheus" === 1);
+
+// console.log(5 < 2 || 5 > 100);
+
+// console.log(!true);
+
+// console.log(!5 > 2);
+
+//TABELA VERDADE -----------------------------------
+
+// EMPTY VALUES --------------------------------------------
+
+
+
+
+
