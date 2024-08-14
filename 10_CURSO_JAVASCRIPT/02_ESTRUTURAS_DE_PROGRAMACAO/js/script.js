81cdba8541819f8009c20245fb69119cfd598c95
AmandaAmbrosioUchoa <amandaambrosio295@gmail.com>
2024-08-14 16:09:58 -0300

    subindo conteúdo das aulas de JS

---
+++ 10_CURSO_JAVASCRIPT/02_ESTRUTURAS_DE_PROGRAMACAO/js/script.js
@@ -0,0 +1,73 @@
+// VARIAVEIS ----------------------------
+
+let nome = "Matheus";
+
+console.log(nome);
+
+// console.log("Matheus");
+
+nome = "Matheus Battisti";
+
+console.log(nome);
+
+const idade = 31;
+
+console.log(idade);
+
+// idade = 32;  ERRADO
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// MAIS SOBRE VARIAVEIS ---------------
+
+let a = 10,
+    b = 20,
+    c = 30;
+
+    console.log(a, b, c);
+
+    const nomecompleto = "Matheus Battisti";
+
+    const NomeCompleto = "João da Silva";
+
+console.log(nomecompleto);
+console.log(NomeCompleto);
+
+
+let _teste = "ok";
+
+let $teste = "Ok";
+
+console.log(_teste, $teste);
+
+// PROMPT --------------------------------
+
+// const age = prompt("Digite sua idade:");
+
+// console.log(`Você tem ${age} anos anos.`);
+
+// ALERT ---------------------------------
+
+// alert("Testando");
+
+// const z = 10;
+
+// alert(`Seu numero é ${z}`);
+
+// FUNÇÕES DO JS MATH.X 
+
+console.log(Math.max(5, 2, 1, 19));
+
+console.log(Math.floor(5.14));
+
+console.log(Math.ceil(5.14));
+
+// FUNÇOES DO console.X 
+
+console.log("teste!");
+
+console.error("erro!");
+
+console.warn("aviso!");
